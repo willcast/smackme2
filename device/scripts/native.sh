2dbd1cf08b9c9fc2685ed0b0a61734621983a53e
Will Castro <castrwilliam@gmail.com>
2013-02-09 08:16:47 -0700

    Fix media size when there's no free VG space + Add support for unsplitting

--- device/scripts/native.sh
+++ device/scripts/native.sh
@@ -24,3 +24,25 @@ install_native() {
 	echo "Extraction complete." >&2
 }
 
+unsplit_native() {
+	if [ ! -e /dev/store/$2-root ]; then
+		echo "Creating root logical volume for $2." >&2
+		lvm.static lvcreate -n $2-root -L ${3}M store
+	fi
+
+	echo "Formatting root filesystem for $2." >&2
+	mkfs.ext3 /dev/store/$2-root 2>/dev/null
+	mkdir /mnt/$2
+	mount -t ext4 /dev/store/$2-root /mnt/$2
+	echo "Complete." >&2
+
+	partname=${1%.1}
+	
+	num=$(ls $partname.* | wc -l)
+
+	echo "Extracting $num split tarchives for $2. This may take upwards of 10 minutes." >&2
+	echo "Please wait..." >&2
+	cat $partname.* | gunzip -c | tar -C /mnt/$2 -x 
+
+	echo "Extraction complete." >&2
+}
